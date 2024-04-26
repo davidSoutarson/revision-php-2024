0d1c4dc91ba2fae82742638a0c5c19d6550932ef
davidSoutarson <triplaxs@gmail.com>
2024-04-26 15:19:25 +0200

    debut de projet

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/masteur.css">
+    <title>Revision php 2024</title>
+</head>
+
+<body>
+    <header class="header bgC_bleux">
+        <h1 class="titre-banner couleur position">Revision php </h1>
+    </header>
+
+    <nav class="conteneur-menu-pricipale">
+        <h1>menu de navigation</h1>
+        <ul classe="menu">
+            <li class="liste">
+                <a class="iteme1" href="">
+                    acceuille
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme1" href="">
+                    html
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme2" href="">
+                    css
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme3" href="">
+                    php
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme3" href="">
+                    javaScript
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme3" href="">
+                    MySql
+                </a>
+            </li>
+            <li class="liste">
+                <a class="iteme3" href="">
+                    Autre
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <main>
+        <article class="ar_php001">
+            <!-- contnue  php -->
+            <h1 class="tire_ar_php">les basse de php</h1>
+            <nav class="sommert-php">
+                <h2> Sommert php</h2>
+                <ul>
+                    <li>
+                        <a href="#" class="som_iteme">
+                            voire...
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="som_iteme">
+                            voire...
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="som_iteme">
+                            voire...
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
+            <h2 class="sous_patie">les variable php</h2>
+            <p>
+                Definition: les variable php:
+            </p>
+            <div class="demonstration">
+                <h3>Demonstration :</h3>
+                <?php
+                echo "bonjour je suis ecrit en php";
+
+                ?>
+            </div>
+        </article>
+        <article class="ar_php002">
+            <!-- contnue  php -->
+            <h1 class="tire_ar_php">php 2</h1>
+        </article>
+        <article class="ar_php003">
+            <!-- contnue  php -->
+            <h1 class="tire_ar_php">php 3</h1>
+        </article>
+        <article class="ar_php004">
+            <!-- contnue  php -->
+            <h1 class="tire_ar_php">php 4</h1>
+        </article>
+        <article class="ar_php005">
+            <!-- contnue  php -->
+            <h1 class="tire_ar_php">php 5</h1>
+        </article>
+
+    </main>
+
+    <footer class="footer bgC_gris">
+        <h2>je suis peid de page </h2>
+    </footer>
+</body>
+
+</html>
